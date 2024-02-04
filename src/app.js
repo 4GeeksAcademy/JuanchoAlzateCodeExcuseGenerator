56452a4b3dd86c77f57e17bbd713a693f9147981
Juan Alzate <139303318+juanchoalzate@users.noreply.github.com>
2024-02-04 04:52:35 +0000

    generador-03-02-2024

--- src/app.js
+++ src/app.js
@@ -1,11 +1,41 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+var imagen = document.getElementById("imagen");
+var Sound = document.getElementById("SoundButtom");
+window.onload = () => {
+  document.querySelector("#btn_excusas").addEventListener("click", () => {
+    Sound.play();
+    imagen.classList.add("active");
+    setTimeout(function() {
+      imagen.classList.remove("active");
+    }, 3000);
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+    document.querySelector("#excuse").innerHTML = generadorExcusas();
+  });
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  console.log("Hello Juancho from the console!");
+};
+
+const generadorExcusas = () => {
+  const pronoun = ["Un", "El"];
+  const subject = ["cartero", "gato", "perro", "conductor", "portero"];
+  const action = ["tomo", "lanzo", "escondió", "me robó", "mordió"];
+  const possession = ["la tarea", "el zapato", "las llaves", "la bicicleta"];
+  const where = ["en la calle", "en mi casa", "en el camino a casa"];
+
+  const proIndex = Math.floor(Math.random() * pronoun.length);
+  const subjIndex = Math.floor(Math.random() * subject.length);
+  const actionIndex = Math.floor(Math.random() * action.length);
+  const possessionIndex = Math.floor(Math.random() * possession.length);
+  const whereIndex = Math.floor(Math.random() * where.length);
+
+  return (
+    pronoun[proIndex] +
+    " " +
+    subject[subjIndex] +
+    " " +
+    action[actionIndex] +
+    " " +
+    possession[possessionIndex] +
+    " " +
+    where[whereIndex]
+  );
 };
